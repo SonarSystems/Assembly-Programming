9801b1460ec3b6c39835366c77b5700914e35936
Sonar Systems <SonarSystems@users.noreply.github.com>
2019-04-18 14:06:48 +0100

    0

---
+++ [4] Variables/Allocating Storage Space for Uninitialized Data/main.asm
@@ -0,0 +1,33 @@
+;nasm 2.11.08
+
+section .data
+    hello:     db 'Hello world!',10    ; 'Hello world!' plus a linefeed character
+    helloLen:  equ $-hello             ; Length of the 'Hello world!' string
+
+section .bss
+    var1: resb 100
+
+section .text
+	global _start
+
+_start:
+	mov eax,4            ; The system call for write (sys_write)
+	mov ebx,1            ; File descriptor 1 - standard output
+	mov ecx,var1        ; Put the offset of hello in ecx
+	mov edx,helloLen     ; helloLen is a constant, so we don't need to say
+	                     ;  mov edx,[helloLen] to get it's actual value
+	int 80h              ; Call the kernel
+
+
+    mov ebp, "hello"
+    mov [var1], ebp
+    mov eax,4            ; The system call for write (sys_write)
+	mov ebx,1            ; File descriptor 1 - standard output
+	mov ecx,var1        ; Put the offset of hello in ecx
+	mov edx,helloLen     ; helloLen is a constant, so we don't need to say
+	                     ;  mov edx,[helloLen] to get it's actual value
+	int 80h              ; Call the kernel
+
+	mov eax,1            ; The system call for exit (sys_exit)
+	mov ebx,0            ; Exit with return code of 0 (no error)
+	int 80h;

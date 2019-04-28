ef205193c91ce96a5a343de0fc4c96b1ab87f921
Sonar Systems <SonarSystems@users.noreply.github.com>
2019-04-28 15:56:19 +0100

    Code

---
+++ [7] Logical Instructions/XOR Instruction/main.asm
@@ -0,0 +1,24 @@
+;nasm 2.11.08
+
+section .data
+    hello:     db 'Hello world!',10    ; 'Hello world!' plus a linefeed character
+    helloLen:  equ $-hello             ; Length of the 'Hello world!' string
+
+section .text
+	global _start
+
+_start:
+    mov esp, 10
+    mov ebp, 3
+    xor esp, ebp
+
+	mov eax,4            ; The system call for write (sys_write)
+	mov ebx,1            ; File descriptor 1 - standard output
+	mov ecx,hello        ; Put the offset of hello in ecx
+	mov edx,esp     ; helloLen is a constant, so we don't need to say
+	                     ;  mov edx,[helloLen] to get it's actual value
+	int 80h              ; Call the kernel
+
+	mov eax,1            ; The system call for exit (sys_exit)
+	mov ebx,0            ; Exit with return code of 0 (no error)
+	int 80h;
